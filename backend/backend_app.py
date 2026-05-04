0d60ede50d4455ac65ba1a1536b62b16e2641c82
Vincent König <vincentkoenig97@gmail.com>
2026-05-04 17:11:43 +0200

    Add POST and DELETE endpoint for blog posts

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,24 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'GET':
+        return jsonify(POSTS)
+
+    elif request.method == 'POST':
+        data = request.json
+        if not data.get('title') or not data.get('content'):
+            return jsonify({"Error": "No title or content"}), 400
+
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post = {
+            "id": new_id,
+            "title": data['title'],
+            "content": data['content']
+        }
+        POSTS.append(new_post)
+        return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
